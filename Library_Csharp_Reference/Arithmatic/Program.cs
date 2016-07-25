7161f080e80e4ff33872bed13fdd530ee3a0b86d
Patrick Dobson <pddobso@gmail.com>
2016-07-25 12:58:30 -0400

    Completed basic types, mid-prog on arithmetic, added imperial march beep-song.

---
+++ Library_Csharp_Reference/Arithmatic/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arithmetic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string notice1 = "This will be a representation of basic arithmetic operations.";
+            Console.Title = "Arithmetic";
+            Console.WriteLine("###################################################################");
+            Console.WriteLine("#");
+            Console.WriteLine($"#  {notice1}");
+            Console.WriteLine("#");
+            Console.WriteLine("###################################################################");
+
+            var x = 1; // while value is a static 32-bit integer (int32), it can be manipulated after this simply using x = 1 etc. (must be within initial 16/32/64)
+
+            var sum = 32 + 10; // addition
+
+            var diff = 52 - 10; // subtraction
+
+            var prod = 21 * 2; // multiplication
+
+            var quot = 168 / 4; // division
+
+            var mod = 42 % 5; // modulus
+
+
+
+
+
+
+
+
+            // Increment-Decrement operations
+
+            x++; // adds 1 to X, does not require value to X
+
+            ++x; // adds 1 to X, after X's value has been determined
+
+            x--; // subtracts 1 from X, does not require value to X
+
+            --x; // subtract 1 from X, after X's value has been determined
+
+
+            x += 42; //
+
+            x -= 42; //
+
+            x *= 42; //
+
+            x /= 42; //
+
+
+
+
+
+
+
+
+            // Comparative functions
+
+            var eq = 10 == 5; // equal
+
+            var ne = 10 != 5; // not equal
+
+            var gt = 10 > 5; // greater than
+
+            var lt = 10 < 5; // less than
+
+            var gte = 10 >= 5; // greater-than or equal
+
+            var lte = 10 <= 5; // less-than or equal
+
+
+
+
+            var condition1 = true; //
+
+            var condition2 = false; //
+
+            var condition3 = false; //
+
+
+            var both = condition1 && condition2; // cond1 and cond2 must be the same value
+
+            var either = condition1 || condition2; // cond1 and cond2 must be 
+
+            var oneOf = condition1 ^ condition2; // cond1 and cond2 must be different values
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Library_Csharp_Reference/BasicTypes/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string notice1 = "This will be a representation of basic type definitions.";
+            Console.Title = "Basic Types";
+            Console.WriteLine("###########################################################");
+            Console.WriteLine("#");
+            Console.WriteLine($"#  {notice1}");
+            Console.WriteLine("#");
+            Console.WriteLine("###########################################################");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+
+            //Basic type declarations:
+
+            char theChar = 'c';
+            string theString = "Heres a string!";
+            int theInteger = 37;
+            double theDouble = 2.7;
+            decimal theDecimal = 7.2m;
+            short theShort = 32767;
+            long theLong = 128;
+            byte theByte = 255;
+            float theFloat = 10.8f; // suffix 'f' on end of number, forces the float type.
+            var theVar = "Any type";
+
+
+            // Reference Types
+
+            //object 0 = new object();
+
+            string s1 = "A regular string."; // normal string
+
+            string s2 = "This is a \r\nmulti-line string."; // \r \n are return-line
+
+            string s3 = @"This is a \r\nverbatim-line string."; // @ character before string, prints exact
+
+            string s4 = $"A string with variables like: \r\n {theFloat} & {theString} & {theInteger}!"; // $ before string results in a parse of the string for variables to be written.
+
+
+
+            Console.WriteLine(s1);
+            Console.WriteLine(" ");
+            Console.WriteLine(s2);
+            Console.WriteLine(" ");
+            Console.WriteLine(s3);
+            Console.WriteLine(" ");
+            Console.WriteLine(s4);
+            Console.WriteLine(" ");
+            Console.ReadLine();
+        }
+    }
+}
